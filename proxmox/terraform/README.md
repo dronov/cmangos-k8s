38d43d45f2c412de4a920c73d3c69927b7c4791e
Mikhail Dronov <mike@dronov.net>
2024-02-17 17:58:37 +0100

    add terraform directory to proxmox structure

---
+++ proxmox/terraform/README.md
@@ -0,0 +1,1 @@
+Terraform configuration to run virtual servers for k8s in Proxmox
---
+++ proxmox/terraform/main.tf
@@ -0,0 +1,39 @@
+resource "proxmox_vm_qemu" "instance" {
+  count       = 5
+  name        = count.index == 3 || count.index == 1 ? "infra${count.index}" : "server${count.index}"
+  target_node = var.proxmox_host
+  clone       = var.ubuntu_template_name
+  os_type     = "linux"
+  cores       = 4
+  sockets     = 1
+  cpu         = "host"
+  memory      = count.index == 3 || count.index == 1 ? 4096 : 2048
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  qemu_os     = "other"
+
+  disk {
+    slot     = 0
+    size     = "50G"
+    type     = "scsi"
+    storage  = "local-lvm"
+    iothread = 0
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = count.index == 3 || count.index == 1 ? "ip=${var.net_prefix}.5${count.index}/24,gw=${var.net_prefix}.1" : "ip=${var.net_prefix}.10${count.index + 1}/24,gw=${var.net_prefix}.1"
+
+  lifecycle {
+    ignore_changes = [network]
+  }
+
+  sshkeys = var.ssh_key
+}
+
+data "sops_file" "secrets" {
+  source_file = "secrets.yaml"
+}
